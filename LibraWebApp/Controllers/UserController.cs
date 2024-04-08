bf03659e0b229b03a0b25d942501d0a2c442be82
Constantin Vizir <vizirconstantin.89@gmail.com>
2024-04-09 00:45:05 +0300

    update get controller added, newtonjson, datatables jquerry pckg updated

--- LibraWebApp/Controllers/UserController.cs
+++ LibraWebApp/Controllers/UserController.cs
@@ -1,107 +1,105 @@
 ﻿using FluentValidation;
-using LibraBll.Abstractions;
 using LibraBll.Abstractions.Repositories;
-using LibraBll.DTOs;
 using LibraBll.DTOs.User;
-using LibraBll.Repositories;
-using LibraBll.Validators.User;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace LibraWebApp.Controllers
 {
-	[Authorize(Roles = "Administrator")]
-	public class UserController : Controller
-	{
-		private readonly IUserRepository _userRepository;
-		private readonly IValidator<AddUserDTO> _createUserValidator;
+    [Authorize(Roles = "Administrator")]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IValidator<AddUserDTO> _createUserValidator;
 
-		public UserController(IUserRepository userRepository, IValidator<AddUserDTO> createUserValidator)
-		{
-			_userRepository = userRepository;
-			_createUserValidator = createUserValidator;
-		}
+        public UserController(IUserRepository userRepository, IValidator<AddUserDTO> createUserValidator)
+        {
+            _userRepository = userRepository;
+            _createUserValidator = createUserValidator;
+        }
 
-		[HttpGet]
-		public ActionResult Index()
-		{
-			var user = User.Identity.Name;
-			return View();
-		}
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var user = User.Identity.Name;
+            return View();
+        }
 
-		[HttpGet]
-		public async Task<ActionResult> GetUserByName(string name)
-		{
-			var userFromDb = await _userRepository.GetUserByNameAsync(name);
-			return View(userFromDb);
-		}
+        [HttpGet]
+        public async Task<ActionResult> GetUserByName(string name)
+        {
+            var userFromDb = await _userRepository.GetUserByNameAsync(name);
+            return View(userFromDb);
+        }
 
-		[Authorize]
-		[HttpGet]
-		public async Task<ActionResult> GetAllUsers()
-		{
-			List<UserDTO> allUsers = await _userRepository.GetAllUsersAsync();
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> GetAllUsers()
+        {
+            List<UserDTO> allUsers = await _userRepository.GetAllUsersAsync();
 
-			if (!allUsers.Any())
-				return null;
+            if (!allUsers.Any())
+                return null;
 
-			return PartialView(allUsers);
-		}
+            return PartialView(allUsers);
+        }
 
-		[Authorize]
-		[HttpGet]
-		public async Task<JsonResult> GetAllUsersJson()
-		{
-			List<UserDTO> allUsers = await _userRepository.GetAllUsersAsync();
+        [Authorize]
+        [HttpGet]
+        public async Task<JsonResult> GetAllUsersJson()
+        {
+            List<UserDTO> allUsers = await _userRepository.GetAllUsersAsync();
 
-			if (!allUsers.Any())
-				return Json(new { }, JsonRequestBehavior.AllowGet);
+            if (!allUsers.Any())
+                return Json(new { }, JsonRequestBehavior.AllowGet);
 
-			return Json(allUsers, JsonRequestBehavior.AllowGet);
-		}
+            return Json(allUsers, JsonRequestBehavior.AllowGet);
+        }
 
-		[HttpGet]
-		public ActionResult AddUser()
-		{
-			return View("AddUser");
-		}
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View("AddUser");
+        }
 
-		[HttpPost]
-		public async Task<ActionResult> AddUser(AddUserDTO user)
-		{
-			var results = _createUserValidator.Validate(user);
-			
-			if (!results.IsValid)
-			{
-				foreach(var failure in results.Errors)
-				{
-					ModelState.AddModelError(failure.PropertyName, failure.ErrorMessage);
-				}
-				return PartialView();
-			}
-			await _userRepository.CreateUser(user);
-			return PartialView();
-		}
+        [HttpPost]
+        public async Task<ActionResult> AddUser(AddUserDTO user)
+        {
+            var results = _createUserValidator.Validate(user);
 
-		[HttpPost]
-		//[Route("User/UpdateUser/{id}")]
-		public async Task<ActionResult> UpdateUser(UserDTO user)
-		{
-			_userRepository.UpdateUser(user);
-			return PartialView();
-		}
+            if (!results.IsValid)
+            {
+                foreach (var failure in results.Errors)
+                {
+                    ModelState.AddModelError(failure.PropertyName, failure.ErrorMessage);
+                }
+                return PartialView();
+            }
+            await _userRepository.CreateUser(user);
+            return PartialView();
+        }
 
-		[HttpPost]
-		[Route("User/DeleteUser/{id}")]
-		public void DeleteUser(int id)
-		{
-			_userRepository.DeleteUser(id);
-			
-		}
-	}
+        [HttpGet]
+        public async Task<ActionResult> UpdateUser(int id)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            return View("~/Views/User/_Edit.cshtml", user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateUser(UserDTO user)
+        {
+            _userRepository.UpdateUser(user);
+            return PartialView();
+        }
+
+        [HttpPost]
+        [Route("User/DeleteUser/{id}")]
+        public void DeleteUser(int id)
+        {
+            _userRepository.DeleteUser(id);
+        }
+    }
 }
