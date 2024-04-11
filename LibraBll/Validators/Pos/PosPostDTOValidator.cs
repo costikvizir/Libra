a072e9d81bb5e6fe65288f9ded2cfe7d78f45212
Constantin Vizir <vizirconstantin.89@gmail.com>
2024-04-11 09:56:34 +0300

    pos validator and pos view changed

--- LibraBll/Validators/Pos/PosPostDTOValidator.cs
+++ LibraBll/Validators/Pos/PosPostDTOValidator.cs
@@ -1,5 +1,4 @@
 ﻿using FluentValidation;
-using LibraBll.Common;
 using LibraBll.DTOs.Pos;
 using System;
 
@@ -7,45 +6,45 @@ namespace LibraBll.Validators.Pos
 {
     public class PosPostDTOValidator : AbstractValidator<PosPostDTO>
     {
-		public PosPostDTOValidator()
-		{
-			RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        public PosPostDTOValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
 
-			RuleFor(x => x.Telephone)
-				.NotEmpty().WithMessage("Missing Telephone")
-				.MinimumLength(9).WithMessage("Not a phone number")
-				.Matches(@"^\+?[0-9]*$").WithMessage("Phone number should contain only numbers");
+            RuleFor(x => x.Telephone)
+                .NotEmpty().WithMessage("Missing Telephone")
+                .MinimumLength(9).WithMessage("Not a phone number")
+                .Matches(@"^\+?[0-9]*$").WithMessage("Phone number should contain only numbers");
 
-			RuleFor(x => x.Telephone)
-				.NotEmpty().WithMessage("Missing Telephone")
-				.MinimumLength(9).WithMessage("Not a phone number")
-				.Matches(@"^\+?[0-9]*$").WithMessage("Phone number should contain only numbers");
+            RuleFor(x => x.Telephone)
+                .NotEmpty().WithMessage("Missing Telephone")
+                .MinimumLength(9).WithMessage("Not a phone number")
+                .Matches(@"^\+?[0-9]*$").WithMessage("Phone number should contain only numbers");
 
-			RuleFor(x => x.Address).NotEmpty().WithMessage("Address is required");
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address is required");
 
-			RuleFor(x => x.City).NotEmpty().WithMessage("City is required");
+            RuleFor(x => x.City).NotEmpty().WithMessage("City is required");
 
-			RuleFor(x => x.Model).NotEmpty().WithMessage("Model is required");
+            RuleFor(x => x.Model).NotEmpty().WithMessage("Model is required");
 
-			RuleFor(x => x.Brand).NotEmpty().WithMessage("Brand is required");
+            RuleFor(x => x.Brand).NotEmpty().WithMessage("Brand is required");
 
-			RuleFor(x => x)
-				.Must(x => Convert.ToInt32(x.MorningClosing) > Convert.ToInt32(x.MorningOpening))
-				.WithMessage("Morning closing must be greater than morning opening");
+            RuleFor(x => x)
+                .Must(x => Convert.ToInt32(x.MorningClosing) > Convert.ToInt32(x.MorningOpening))
+                .WithMessage("Morning closing must be greater than morning opening");
 
-			//RuleFor(x => x)
-			//	.Must(x => Convert.ToInt32(x.AfternoonOpening) > Convert.ToInt32(x.MorningClosing))
-			//	.WithMessage("Morning closing must be greater than morning opening");
+            RuleFor(x => x)
+                .Must(x => Convert.ToInt32(x.AfternoonOpening) > Convert.ToInt32(x.MorningClosing))
+                .WithMessage("Morning closing must be greater than morning opening");
 
-			//RuleFor(x => x)
-			//	.Must(x => Convert.ToInt32(x.AfternoonOpening) > Convert.ToInt32(x.AfternoonOpening))
-			//	.WithMessage("Morning closing must be greater than morning opening");
+            RuleFor(x => x)
+                .Must(x => Convert.ToInt32(x.AfternoonOpening) > Convert.ToInt32(x.AfternoonOpening))
+                .WithMessage("Morning closing must be greater than morning opening");
 
-			// RuleFor(x => x.ConnectionType).NotEmpty().WithMessage("Connection Type is required");
-			//RuleFor(x => x.MorningOpening).NotEmpty().WithMessage("Morning Opening is required");
-			//RuleFor(x => Convert.ToInt32(x.MorningClosing) > Convert.ToInt32(x.MorningOpening))
-			//RuleFor(x => x.AfternoonOpening).NotEmpty().WithMessage("Afternoon Opening is required");
-			//RuleFor(x => x.AfternoonClosing).NotEmpty().WithMessage("Afternoon Closing is required");
-		}
-	}
+            // RuleFor(x => x.ConnectionType).NotEmpty().WithMessage("Connection Type is required");
+            //RuleFor(x => x.MorningOpening).NotEmpty().WithMessage("Morning Opening is required");
+            //RuleFor(x => Convert.ToInt32(x.MorningClosing) > Convert.ToInt32(x.MorningOpening))
+            //RuleFor(x => x.AfternoonOpening).NotEmpty().WithMessage("Afternoon Opening is required");
+            //RuleFor(x => x.AfternoonClosing).NotEmpty().WithMessage("Afternoon Closing is required");
+        }
+    }
 }
